387cb377957d2d05acd52c49e56537425019fae8
Enrico Willemse <enricowillemse.was@gmail.com>
2022-04-27 15:34:00 +0000

    Update

--- src/smart-task-component.vue
+++ src/smart-task-component.vue
@@ -1,28 +1,3 @@
-<script lang="ts">
-import { defineComponent } from "vue";
-
-
-export default /*#__PURE__*/ defineComponent({
-  name: "SmartTaskComponent", // vue component name
-  props: ['componentState', 'componentStatus'],
-  data() {
-    return { model: {} };
-  },
-  computed: {},
-  methods: {
-    async submitForm() {
-      this.$emit("execute-smart-task", this.model);
-    },
-  },
-  created() {
-    if (this.componentState) {
-      this.model = this.componentState;
-    }
-  },
-  setup() {},
-});
-</script>
-
 <template>
   <form>
     <div class="my-5">
@@ -37,3 +12,50 @@ export default /*#__PURE__*/ defineComponent({
   </form>
 </template>
 
+<template> 
+    <div class="col-md-12">
+     <div class="fv-row mb-5">
+       <template v-for="task in model.taskChecklist">
+            <div class="d-flex align-items-center mb-2 py-3 bg-white">
+												<!--begin::Bullet-->
+												<span class="bullet bullet-vertical h-40px"></span>
+												<!--end::Bullet-->
+												<!--begin::Checkbox-->
+												<div class="form-check form-check-custom form-check-solid mx-5">
+													<input class="form-check-input" type="checkbox" value="">
+												</div>
+												<!--end::Checkbox-->
+												<!--begin::Description-->
+												<div class="flex-grow-1">
+													<a href="#" class="text-gray-800 text-hover-primary fw-bolder fs-6">{{task.taskName}}</a>
+													<span class="text-muted fw-bold d-block" v-html="task.taskDescription"></span>
+												</div>
+						 </div>
+        </template>
+    </div>
+  <button class="btn btn-sm btn-success">Mark all as complete</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SmartTaskComponent",
+  props: ['componentState', 'componentStatus'],
+  data() {
+    return { model: {} };
+  },
+  computed: {},
+  methods: {
+    async submitForm() {
+      this.$emit("execute-smart-task", this.model);
+    }
+  },
+  created() {
+    if (this.componentState) {
+      this.model = this.componentState;
+    }
+  },
+  setup() {},
+});
+</script>
